8ea5d5d4b85c33bffb23f4f0fc72438e177885cb
juliagsy <67888047+juliagsy@users.noreply.github.com>
2024-01-08 14:43:30 +0000

    update model and docs

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Imagic
+
+## Introduction
+
+## Usage
+
+```python
+from transformers import AutoImageProcessor
+from imagic.hf import ImagicModel
+
+vit_proc = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k", low_cpu_mem_usage=True)
+
+i2m = ImagicModel.from_pretrained("juliagsy/imagic")
+```
+
+## Image-conditioned Music Generation
+
+### Generation
+
+```python
+from IPython.display import Audio
+from IPython.core.display import display
+
+input_img = "<your-image>"
+
+img = vit_proc(input_img, do_rescale=False, return_tensors="pt")
+img = img.to("cuda")
+
+gen_wav = i2m.generate(img, guidance_scale=3)
+sampling_rate = i2m.model.musicgen.config.audio_encoder.sampling_rate
+display(Audio(gen_wav[0].cpu().numpy(), rate=sampling_rate))
+```
+
+### Example
+
+Example image:
+
+![example image 1](examples/img_5.png)
+
+[Output wav](examples/wav_5.wav)
+
---
+++ imagic/imagic.py
@@ -0,0 +1,84 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from transformers import AutoProcessor, MusicgenForConditionalGeneration, AutoImageProcessor, ViTModel
+
+
+class Im2Mu(nn.Module):
+  def __init__(self, embed_dims=768, seq_len=64):
+    super(Im2Mu, self).__init__()
+
+    self.musicgen = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+    self.muvis = MuVisModel.from_pretrained("juliagsy/muvis").model.vit
+
+    self.loss_ce = nn.CrossEntropyLoss(label_smoothing=0.1, ignore_index=-100)
+    self.img_lin = nn.Linear(197, 256)
+
+
+  def shift_right(self, input_ids):
+    shifted_input_ids = torch.zeros_like(input_ids)
+    shifted_input_ids[:, 1:] = input_ids[:, :-1].clone()
+    shifted_input_ids[:, 0] = 0
+    return shifted_input_ids
+
+
+  def forward(self, img, wav):
+    img_e = self.muvis(**img)["last_hidden_state"]
+    img_embeds = self.musicgen.get_encoder()(
+        inputs_embeds=img_e
+    )["last_hidden_state"]
+    img_embeds = img_embeds.permute(0, 2, 1)
+    img_embeds = self.img_lin(img_embeds)
+    img_embeds = img_embeds.permute(0, 2, 1)
+
+    wav_tokens = self.musicgen.get_audio_encoder().encode(
+        **wav,
+    )["audio_codes"]
+    wav_size = wav_tokens.size()
+    wav_tokens = wav_tokens.view((wav_size[1] * wav_size[2], wav_size[-1]))
+    wav_tokens = self.shift_right(wav_tokens)
+
+    ret = self.musicgen(
+        decoder_input_ids=wav_tokens,
+        encoder_outputs=(img_embeds,),
+    )
+    loss = self.loss_ce(ret.logits.view(-1, self.musicgen.config.audio_encoder.codebook_size), wav_tokens.view(-1))
+    return loss
+
+
+  def generate(self, img, wav=None, guidance_scale=3, max_new_tokens=256, device="cpu"):
+    img_embeds = self.muvis(**img)["last_hidden_state"]
+    img_embeds = img_embeds.permute(0, 2, 1)
+    img_embeds = self.img_lin(img_embeds)
+    img_embeds = img_embeds.permute(0, 2, 1)
+
+    img_embeds = self.musicgen.get_encoder()(
+        inputs_embeds=img_embeds
+    )["last_hidden_state"]
+
+    if wav is not None:
+      input_ids = self.musicgen.get_audio_encoder().encode(
+        **wav,
+      )["audio_codes"]
+      wav_size = input_ids.size()
+      input_ids = input_ids.view((wav_size[1] * wav_size[2], wav_size[-1]))
+      input_ids = self.shift_right(input_ids)
+      ret = self.musicgen.generate(
+          decoder_input_ids=input_ids,
+          encoder_outputs=(img_embeds,),
+          do_sample=True,
+          guidance_scale=guidance_scale,
+          max_new_tokens=256,
+      )
+    else:
+      input_ids = torch.zeros((4, 1)).long().to(device)
+      decoder_attention_mask = torch.ones((img_embeds.size(0), 1)).long().to(device)
+      ret = self.musicgen.generate(
+          decoder_input_ids=input_ids,
+          decoder_attention_mask=decoder_attention_mask,
+          encoder_outputs=(img_embeds,),
+          do_sample=True,
+          guidance_scale=guidance_scale,
+          max_new_tokens=max_new_tokens,
+      )
+    return ret
